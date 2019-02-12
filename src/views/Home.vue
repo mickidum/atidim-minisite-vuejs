dace0f111ad7868aa8e13cb117eb8b6270b68a42
Michael <michael@zur.local>
2019-02-12 18:03:31 +0200

    home page without footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,13 +5,79 @@
 		</div>
 		<div class="filter">
 			<div class="container">
-				<h1 class="text-green pt-3 pb-2">משרדים להשכרה</h1>
+				<h1 class="text-green my-3 header-border">משרדים להשכרה</h1>
 			</div>
 			<img src="../assets/home2.jpg" alt="home2" />
 		</div>
 		<div class="short-form">
 			<div class="container">
 				<h2>צור קשר</h2>
+				<ShortForm />
+			</div>
+		</div>
+		<div class="asakim">
+			<div class="container">
+				<h2 class="text-green header-border mb-4">
+					עסקים מבוקשים לפארק עתידים
+				</h2>
+				<b-row>
+					<b-col md="4">
+						<img class="w-100" src="../assets/asakim/a-1.jpg" alt="גאדג׳טים" />
+						<h4 class="text-center bg-dark text-white py-1 mt-1 mb-3">
+							גאדג׳טים
+						</h4>
+					</b-col>
+					<b-col md="4">
+						<img
+							class="w-100"
+							src="../assets/asakim/a-2.jpg"
+							alt="מעבדת סלולר"
+						/>
+						<h4 class="text-center bg-dark text-white py-1 mt-1 mb-3">
+							מעבדת סלולר
+						</h4>
+					</b-col>
+					<b-col md="4">
+						<img class="w-100" src="../assets/asakim/a-3.jpg" alt="סניף בנק" />
+						<h4 class="text-center bg-dark text-white py-1 mt-1 mb-3">
+							סניף בנק
+						</h4>
+					</b-col>
+
+					<div class="w-100"></div>
+
+					<b-col md="4">
+						<img
+							class="w-100"
+							src="../assets/asakim/a-4.jpg"
+							alt="אופניים חשמליים"
+						/>
+						<h4 class="text-center bg-dark text-white py-1 mt-1 mb-3">
+							אופניים חשמליים
+						</h4>
+					</b-col>
+					<b-col md="4">
+						<img
+							class="w-100"
+							src="../assets/asakim/a-5.jpg"
+							alt="ציוד משרדי"
+						/>
+						<h4 class="text-center bg-dark text-white py-1 mt-1 mb-3">
+							ציוד משרדי
+						</h4>
+					</b-col>
+					<b-col md="4">
+						<img class="w-100" src="../assets/asakim/a-6.jpg" alt="ניקוי יבש" />
+						<h4 class="text-center bg-dark text-white py-1 mt-1 mb-3">
+							ניקוי יבש
+						</h4>
+					</b-col>
+				</b-row>
+			</div>
+		</div>
+		<div class="short-form bottom-form">
+			<div class="container">
+				<h2>צרו קשר 8485* <span> / </span> 050-4455074</h2>
 				<ShortForm />
 			</div>
 		</div>
@@ -29,41 +95,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.short-form {
-	position: relative;
-	padding: 40px 0;
-	background-color: #006634;
-	h2 {
-		text-align: center;
-		color: #fff;
-		margin-bottom: 20px;
-		font-size: 2.8rem;
-	}
-	input,
-	button {
-		height: 60px;
-		border-radius: 0;
-		border: solid 4px white !important;
-		color: white;
-		font-weight: bold;
-		font-size: 24px;
-		line-height: 1;
-	}
-	input {
-		background-color: #000;
-	}
-	button {
-	}
-	::placeholder {
-		color: #fff;
-		font-weight: bold;
-		font-size: 24px;
-	}
-}
-.filter {
-	img {
-		opacity: 0.8;
-	}
-}
-</style>
+<style lang="scss"></style>
