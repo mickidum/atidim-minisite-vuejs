9105a8f592f2f2ed51ea12a4986ebf1b1979b73b
Michael <michael@zur.local>
2019-02-13 11:57:37 +0200

    image fix

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <div class="main-menu">
       <MainMenu im="logo.png" />
     </div>
-    <router-view />
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
     <div class="short-form bottom-form">
       <div class="container">
         <h2>צרו קשר 8485* <span> / </span> 050-4455074</h2>
